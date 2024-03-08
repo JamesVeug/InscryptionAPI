52b1db7045cb6f06f4ff4ba3d58a6772b617f1b9
James <james.veugelaers@outlook.co.nz>
2024-03-09 11:45:44 +1300

    DialogueEventStrings is now serializable

--- InscryptionAPI/TalkingCards/Create/DialogueEventStrings.cs
+++ InscryptionAPI/TalkingCards/Create/DialogueEventStrings.cs
@@ -1,4 +1,5 @@
 using InscryptionAPI.Dialogue;
+using UnityEngine;
 
 #nullable enable
 
@@ -7,21 +8,49 @@ namespace InscryptionAPI.TalkingCards.Create;
 [Serializable]
 public class DialogueEventStrings
 {
-    public string eventName { get; set; }
-    public string[] mainLines { get; set; }
-    public string[][] repeatLines { get; set; }
-
-    public DialogueEventStrings(string eventName, string[] mainLines, string[][] repeatLines)
+    public string eventName
     {
-        this.eventName = eventName;
-        this.mainLines = mainLines;
-        this.repeatLines = repeatLines;
+        get => m_eventName;
+        set => m_eventName = value;
+    }
+    
+    public string[] mainLines
+    {
+        get => m_mainLines;
+        set => m_mainLines = value;
+    }
+    
+    public string[][] repeatLines
+    {
+        get => m_repeatLines;
+        set => m_repeatLines = value;
     }
 
-    public DialogueEvent CreateEvent(string cardName) => DialogueManager.GenerateEvent(
+    [SerializeField]
+    private string m_eventName;
+    
+    [SerializeField]
+    private string[] m_mainLines;
+    
+    [SerializeField]
+    private string[][] m_repeatLines;
+    
+    public DialogueEventStrings(string eventName, string[] mainLines, string[][] repeatLines)
+    {
+        m_eventName = eventName;
+        m_mainLines = mainLines;
+        m_repeatLines = repeatLines;
+    }
+
+    public DialogueEvent CreateEvent(string cardName)
+    {
+        List<CustomLine> lines = mainLines.Select(x => (CustomLine)x).ToList();
+        List<List<CustomLine>> repeatedLines = repeatLines.Select(x => x.Select(y => (CustomLine)y).ToList()).ToList();
+        return DialogueManager.GenerateEvent(
             InscryptionAPIPlugin.ModGUID,
             $"{cardName}_{eventName}",
-            mainLines.Select(x => (CustomLine)x).ToList(),
-            repeatLines.Select(x => x.Select(y => (CustomLine)y).ToList()).ToList()
+            lines,
+            repeatedLines
         );
+    }
 }
